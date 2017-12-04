3d50dc668fbfb2b75232e004ddcdfb7b806dd59f
superflyc <r0ckstar>
2017-12-03 20:33:49 -0600

    basic cars display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {Car} from './interfaces/car';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'CMW Firestore';
+  carCollection: AngularFirestoreCollection<Car>;
+  cars: Observable<Car[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.carCollection = this.afs.collection<Car>('cars');
+    this.cars = this.carCollection.valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 
 
 @NgModule({
@@ -10,7 +12,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      AngularFireModule.initializeApp({
+          apiKey: 'AIzaSyC2CUd7zcUII_XmFBFqT-b8zldDrkqios0',
+          authDomain: 'cmw-firebase.firebaseapp.com',
+          databaseURL: 'https://cmw-firebase.firebaseio.com',
+          projectId: 'cmw-firebase',
+      }),
+      AngularFirestoreModule.enablePersistence()
   ],
   providers: [],
   bootstrap: [AppComponent]
